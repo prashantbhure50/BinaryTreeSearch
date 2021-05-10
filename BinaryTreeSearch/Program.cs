be122ae34abda6b078643366d0b7a576c4c394c8
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-10 22:37:51 +0530

    Add project files.

---
+++ BinaryTreeSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTreeSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to Binary Search Tree Problem");
+        }
+    }
+}
